ef71cedcc2e295c2e1d6a5ef1d3513ec1cd9b125
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2024-06-01 16:43:10 +0000

    Fix Node and LinkedList classes, add prepend and append Methods nextNode and fullList arguments do not seem to be necessary in the constructors, will need to verify later

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
-class LinkedList {
-    constructor(value) {
-        this._value = value;
-    }
-    append(value) {
-
-    }
-}
-
 class Node {
-    constructor(value, nextNode) {
-        this._value = value; // is this correct or should they be this._value = null?
-        this._nextNode = value;
+    constructor(value=null, nextNode=null) {
+        this._value = value; 
+        // this._nextNode = nextNode;
     }    
 }
+
+
+class LinkedList {
+    constructor(fullList) {
+        // this._fullList = fullList;
+        this._head = null;
+    }
+
+    // let head = new Node();
+
+    prepend(value) { 
+        this._head = new Node(value)
+        return this._head;
+    }
+
+    append(value) {
+        if (this._head == null) {
+            console.log("pasta")
+            this.prepend(value)
+        } else {
+            console.log("tuna")
+            let temporary = this._head;
+            while (temporary.nextNode != null) {
+                temporary = temporary.nextNode;
+            }
+            temporary.nextNode = new Node(value);
+        }
+    }
+}
+
+let test = new LinkedList;
+test.prepend("prepend1")
+test.append("append1")
+test.append("append2")
+
+console.log(test)
+
+
+
+
 
 // const list = {
 //     head: {
